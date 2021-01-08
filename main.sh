f1357821e38f97109a400ca2bde2956bcabd3f26
demian-at-066956696796 <demian@kcd.co.kr>
2021-01-08 16:23:02 +0900

    enhance: Add copy-zshrc.sh

--- main.sh
+++ main.sh
@@ -2,6 +2,7 @@
 
 read -n 1 -r -p "Do you want to copy .gitconfig file [Y/n]:" need_gitconfig
 read -n 1 -r -p "Do you want to copy .vimrc file [Y/n]:" need_vimconfig
+read -n 1 -r -p "Do you want to copy .zshrc file [Y/n]:" need_zshrc
 
 ./scripts/init.sh
 
@@ -11,6 +12,10 @@ fi
 
 if [[ $need_vimconfig =~ ^[Yy]$ ]]; then
   ./scripts/copy-vimrc.sh
+fi
+
+if [[ $need_zshrc =~ ^[Yy]$ ]]; then
+  ./scripts/copy-zshrc.sh
 fi
 
 ./scripts/install.sh
